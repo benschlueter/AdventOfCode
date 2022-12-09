397ecd0c31f0c589cef6a77ab7160f3724e3e773
Benedict Schlueter <benedictschlueter@gmail.com>
2022-12-09 12:18:33 +0100

    day 9

--- Day_9/main.go
+++ Day_9/main.go
@@ -16,45 +16,32 @@ func abs(x int) int {
 	return x
 }
 
-func movByOne(newPosH, posT *[2]int, positions map[string]struct{}) {
+func movByOne(newPosH, posT *[2]int, positions map[string]struct{}, tail bool) {
+	x, y := newPosH[0]-posT[0], newPosH[1]-posT[1]
 	if (abs(posT[0]-newPosH[0]) == 2) && (abs(posT[1]-newPosH[1]) == 1) ||
 		(abs(posT[0]-newPosH[0]) == 1) && (abs(posT[1]-newPosH[1]) == 2) {
-		x, y := newPosH[0]-posT[0], newPosH[1]-posT[1]
-		if abs(x) == 2 {
-			posT[0] += (x / 2)
-			posT[1] += y
-		}
-		if abs(y) == 2 {
-			posT[0] += (x)
-			posT[1] += (y / 2)
-		}
-		fmt.Println("update T", posT)
+		posT[0] += (x / abs(x))
+		posT[1] += (y / abs(y))
 	}
 	if (abs(posT[0]-newPosH[0]) == 2) || (abs(posT[1]-newPosH[1]) == 2) {
-		x, y := newPosH[0]-posT[0], newPosH[1]-posT[1]
 		posT[0] += (x / 2)
 		posT[1] += (y / 2)
-		fmt.Println("update T", posT)
 	}
-	positions[fmt.Sprintf("%d:%d", posT[0], posT[1])] = struct{}{}
-}
-
-func move(posH, newPosH, posT *[2]int, positions map[string]struct{}) {
-	x, y := newPosH[0]-posH[0], newPosH[1]-posH[1]
-
-	for i := 1; i <= x; i++ {
-		movByOne(&[2]int{posH[0] + i, posH[1]}, posT, positions)
-	}
-	for i := -1; i >= x; i-- {
-		movByOne(&[2]int{posH[0] + i, posH[1]}, posT, positions)
-	}
-	for i := 1; i <= y; i++ {
-		movByOne(&[2]int{posH[0], posH[1] + i}, posT, positions)
-	}
-	for i := -1; i >= y; i-- {
-		movByOne(&[2]int{posH[0], posH[1] + i}, posT, positions)
+	if tail {
+		// fmt.Println("update T", newPosH, posT)
+		positions[fmt.Sprintf("%d:%d", posT[0], posT[1])] = struct{}{}
 	}
 }
+
+func move(nodes *[lengthTailPlusOne][2]int, x, y int, positions map[string]struct{}) {
+	nodes[0][0] += x
+	nodes[0][1] += y
+	for i := 1; i < lengthTailPlusOne; i++ {
+		movByOne(&nodes[i-1], &nodes[i], positions, i == (lengthTailPlusOne-1))
+	}
+}
+
+const lengthTailPlusOne = 10
 
 func main() {
 	f, err := os.Open("input.txt")
@@ -62,12 +49,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
 	positions := make(map[string]struct{})
-	posH := [2]int{0, 0}
-	posT := [2]int{0, 0}
-	newPosH := [2]int{0, 0}
+	nodes := [lengthTailPlusOne][2]int{}
+	for _, v := range nodes {
+		v[0] = 0
+		v[1] = 0
+	}
 
+	var x, y int
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -75,20 +64,26 @@ func main() {
 		value, _ := strconv.Atoi(splitline[1])
 		switch splitline[0] {
 		case "R":
-			newPosH[0] += value
+			x, y = 1, 0
 		case "L":
-			newPosH[0] -= value
+			x, y = -1, 0
 		case "U":
-			newPosH[1] += value
+			x, y = 0, 1
 		case "D":
-			newPosH[1] -= value
+			x, y = 0, -1
 		}
-		move(&posH, &newPosH, &posT, positions)
-		fmt.Println(posT, newPosH)
-		posH[0] = newPosH[0]
-		posH[1] = newPosH[1]
+
+		// fmt.Println(nodes)
+
+		fmt.Println(x, y, value)
+		for i := 0; i < value; i++ {
+			// x, y := posH[0]-old[0], posH[1]-old[1]
+			move(&nodes, x, y, positions)
+		}
+
 	}
 
+	fmt.Println(1 == 1)
 	fmt.Println(len(positions))
 
 	if err := scanner.Err(); err != nil {
